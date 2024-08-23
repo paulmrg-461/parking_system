81ea2ee1cb4ee9b394d8fc57a8e658228503b064
paulmrg-461 <paulmrg461@gmail.com>
2024-08-22 20:25:01 -0500

    Feat: Core: Firebase options setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:parking_system/config/routes/app_routes.dart';
 import 'package:parking_system/config/themes/custom_theme.dart';
+import 'package:parking_system/core/firebase/firebase_options.dart';
+import 'core/service_locator.dart' as di;
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  di.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
